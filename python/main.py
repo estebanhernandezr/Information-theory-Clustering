32357ae11cc929f1a9057267ffbf00dc998214ba
estebanhernandezr <38502029+estebanhernandezr@users.noreply.github.com>
2022-06-14 18:06:40 -0500

    proofs and debugging

--- python/main.py
+++ python/main.py
@@ -7,7 +7,7 @@ from Phylogenetic_tree_constructor import Phylogenetic_tree
 
 search = 2000
 ahead = 1000
-codificador = CoderLZ77(search, ahead, alphabet=['0', '1', '2', '3', '4', '5', '6'])
+codificador = CoderLZ77(search, ahead, alphabet=['0', '1'])
 
 filogenetic = Phylogenetic_tree()
 filogenetic.build(codificador)
