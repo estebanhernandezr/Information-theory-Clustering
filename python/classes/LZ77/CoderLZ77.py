fa7f2e27d82d419bb5aac64046b14f29a1ed90bf
estebanhernandezr <38502029+estebanhernandezr@users.noreply.github.com>
2022-06-13 19:22:38 -0500

    included a main.py file

--- python/classes/LZ77/CoderLZ77.py
+++ python/classes/LZ77/CoderLZ77.py
@@ -1,22 +1,22 @@
 import sys
 sys.path.insert(0, 'python/functions')
 
+import slate3k as slate
 from typing import List, Tuple
-from fixed_length_codeword import codeWord
+from fixed_length_codeword import code_word
 from sliding_window_mechanism import sliding_window_reproducible_extension
+
 
 class CoderLZ77:
 
-    def __init__(self, n: int, l: int):
+    def __init__(self, n: int, l: int, alphabet: List):
         self.n: int = n
         self.l: int = l
+        self.alphabet = alphabet
+        self.codified_string = ''
 
     def codify(self, string: str, symb: str):
         reproducible_extensions: List[Tuple] = sliding_window_reproducible_extension(string, symb, self.n, self.l)
         for reproducible_extension in reproducible_extensions:
             pos, size, char = reproducible_extension
-            print(codeWord(pos, size, char, self.n, self.l))
-
-
-codificador = CoderLZ77(10, 5)
-codificador.codify('aaaaaaaaaaaaaaaaaaaaaaa', 'b')
+            self.codified_string += code_word(pos, size, char, self.n, self.l, self.alphabet)
--- python/functions/fixed_length_codeword.py
+++ python/functions/fixed_length_codeword.py
@@ -11,12 +11,6 @@ def d2r(radix, decnum, current):
 def dec2radix(radix, decnum):
     return d2r(radix, decnum, '')
 
-def radix2dec(radix, rnum):
-    dec = 0
-    for i in range(0,len(rnum)):
-        dec += int(rnum[len(rnum)-i-1])*(radix**i)
-    return dec
-
 def d2a(alphabet, decnum, current):
     radix = len(alphabet)
     res = decnum % radix
@@ -29,8 +23,14 @@ def d2a(alphabet, decnum, current):
 def dec2alpha(alphabet, decnum):
     return d2a(alphabet, decnum, '')
 
-def codeWord(pos, size, char, n, l):
-    cd = dec2alpha(['0', '1'], pos).rjust(int(np.ceil(np.log(n-l) / np.log(2))), '0')
-    cd += dec2alpha(['0', '1'], size).rjust(int(np.ceil(np.log(l) / np.log(2))), '0')
+def radix2dec(radix, rnum):
+    dec = 0
+    for i in range(0,len(rnum)):
+        dec += int(rnum[len(rnum)-i-1])*(radix**i)
+    return dec
+
+def code_word(pos, size, char, n, l, alphabet):
+    cd = dec2alpha(alphabet, pos).rjust(int(np.ceil(np.log(n-l) / np.log(len(alphabet)))), alphabet[0])
+    cd += dec2alpha(alphabet, size).rjust(int(np.ceil(np.log(l) / np.log(len(alphabet)))), alphabet[0])
     cd += char
     return cd
