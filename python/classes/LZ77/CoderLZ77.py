e457e8540bc990df4f84b913683d0fc567421cf0
estebanhernandezr <38502029+estebanhernandezr@users.noreply.github.com>
2022-06-16 19:32:04 -0500

    changed from manual to python reproducible extension

--- python/classes/LZ77/CoderLZ77.py
+++ python/classes/LZ77/CoderLZ77.py
@@ -5,7 +5,7 @@ import math
 from typing import List, Tuple
 from fixed_length_codeword import code_word
 from auxiliar_functions import generate_combinations_wrapper
-from manual_sliding_window_mechanism import sliding_window_reproducible_extension
+from python_sliding_window_mechanism import sliding_window_reproducible_extension
 
 """
     Function: LZ77 codifier class.
