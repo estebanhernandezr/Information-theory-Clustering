afc807d0c15269cbe980a78f2981b61fd3524b88
estebanhernandezr <38502029+estebanhernandezr@users.noreply.github.com>
2022-07-27 12:43:48 -0500

    hartley information content derived coding implementation

--- python/compressors/lempel_ziv_1977/run.py
+++ python/compressors/lempel_ziv_1977/run.py
@@ -7,7 +7,7 @@ from Decompressor import Decompressor
 
 def run():
     compresor = Compressor(10, 5, ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
-    mensaje_comprimido = compresor.compress('universidad del rosario sede centro bogota colombia', symb='_')
+    mensaje_comprimido = compresor.compress('abababababababab', symb='_')
     print(mensaje_comprimido)
 
     decompresor = Decompressor(10, 5, ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
---
+++ python/transmitters/hartley/encoder/Encoder.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.insert(0, 'python/transmitters/hartley/encoder/functions')
+
+import numpy as np
+
+from fixed_length_code import generate_combinations_wrapper
+
+class Encoder:
+    def __init__(self, source_alphabet, channel_alphabet):
+        self.alpha_A = source_alphabet
+        self.alpha_B = channel_alphabet
+        self.codewords = None
+        self.build_codewords()
+
+    def build_codewords(self):
+        codeword_len = int(np.ceil(np.log(len(self.alpha_A)) / np.log(len(self.alpha_B))))
+        codewords = generate_combinations_wrapper(self.alpha_B, codeword_len)
+        self.codewords = codewords
+
+    def encode(self, string):
+        codified_string = ''
+        for a in string:
+            idx = self.alpha_A.index(a)
+            print(self.codewords[idx])
+            codified_string += self.codewords[idx]
+        return codified_string
+
+codificador = Encoder(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], ['0', '1'])
+print(codificador.encode(['a', 'b', 'b', 'a', 'c']))
